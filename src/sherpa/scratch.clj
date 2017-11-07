8c4e8f5cd8062294b36274367370599934a50ed1
Abhinav Sharma <abhi18av@gmail.com>
2017-11-07 22:31:53 +0530

    added clojure files for all sections

--- src/sherpa/scratch.clj
+++ src/sherpa/scratch.clj
@@ -19,7 +19,7 @@
 
 
 (client/post campfire
-             {:body "{\"content\": \"Delightful\"}"
+             {:body "{\"content\": \"This is now working!\"}"
               :content-type :json})
 
 
@@ -28,3 +28,18 @@
 ;; HTTPIE experiments
 
 http POST  https://3.basecamp.com/3842603/integrations/daJ8YotkogjtYejB7JhWgFHA/buckets/5152297/chats/710484221/lines content='hello from httpie'
+
+
+;; Get projects
+
+
+(def res (client/get "https://3.basecamp.com/3842603/projects.json"
+            {
+:headers 
+            ; :body "{\"content\": \"This is now working!\"}"
+              :content-type :json}))
+
+
+;; curl command
+
+curl -s -H "Authorization: Bearer $ACCESS_TOKEN" https://3.basecampapi.com/$ACCOUNT_ID/projects.json
