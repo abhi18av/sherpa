df12fcbd6c3850831c5701e70948d294355a4bcb
Abhinav Sharma <abhi18av@gmail.com>
2017-11-07 13:27:46 +0530

    added a scratch file for experimentation

--- project.clj
+++ project.clj
@@ -3,19 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-
-                [org.clojure/clojure "1.9.0-beta4"]
-				[im.chit/hara "2.5.10"]
-				[im.chit/lucid "1.3.13"]
-				[com.rpl/specter "1.0.4"]
-				[prismatic/plumbing "0.5.4"] 
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+                 [im.chit/hara "2.5.10"]
+                 [im.chit/lucid "1.3.13"]
+                 [com.rpl/specter "1.0.4"]
+                 [prismatic/plumbing "0.5.4"]
 ;; for interacting with basecamp API
-
-[clj-http "3.7.0"]
-
-
-  ]
+                 [clj-http "3.7.0"]
+;; for logging purposes
+                 [com.fzakaria/slf4j-timbre "0.3.7"]]
   :main ^:skip-aot sherpa.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
